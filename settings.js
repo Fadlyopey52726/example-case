1501a4eda8a63b3ce05a9c00dbd55edeae5d58ee
Fadlyopey52726 <150658996+Fadlyopey52726@users.noreply.github.com>
2023-11-18 01:41:08 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ global.APIKeys = {
 	'https://api.zeeoneofc.my.id': 'Your Key',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
+global.namabot = "Bot Kyra"
+global.namaowner = "Fadly"
+global.packname = Kyra
+global.author = Kyra anak mama
 global.myweb = "https://s.id/zeeoneofc"
 global.footer_text = "© " + namabot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.owner = ['6287716424010']
 global.pairingNumber = "" //pairing number to install bot 
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
